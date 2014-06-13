68df934db26aeca43eeb3614786304b286b14002
Steven Dobek <sdobek47@gmail.com>
2014-06-13 13:55:28 -0700

    Login screen view.

--- gen/com/sdobek/storitime/R.java
+++ gen/com/sdobek/storitime/R.java
@@ -24,18 +24,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int btn_signinLogin=0x7f080003;
+        public static final int editText2=0x7f080002;
+        public static final int et_usernameLogin=0x7f080001;
+        public static final int iv_iconLogin=0x7f080000;
+        public static final int tv_forgotPassword=0x7f080004;
+        public static final int tv_gotoSignup=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_timeline=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_timeline=0x7f030001;
     }
     public static final class menu {
-        public static final int timeline=0x7f070000;
+        public static final int login=0x7f070000;
+        public static final int timeline=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int clicktosignup=0x7f050007;
+        public static final int email=0x7f050009;
+        public static final int forgotPassword=0x7f050008;
         public static final int hello_world=0x7f050002;
+        public static final int password=0x7f050005;
+        public static final int signin=0x7f050006;
+        public static final int title_activity_login=0x7f050003;
+        public static final int username=0x7f050004;
     }
     public static final class style {
         /** 
--- src/com/sdobek/storitime/LoginActivity.java
+++ src/com/sdobek/storitime/LoginActivity.java
@@ -1,26 +1,21 @@
 package com.sdobek.storitime;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
 
-import com.parse.Parse;
-import com.parse.ParseObject;
-
-public class TimelineActivity extends Activity {
+public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Parse.initialize(this, "h0YIg8GPxufMoiNZR6llo3TrdG4z52d2joYr20bl", "XDDlR6ccr1fwBAp9HHa7Jnx1wFthWdPzKQaS1dF3");
-		setContentView(R.layout.activity_timeline);
-		
+		setContentView(R.layout.activity_login);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.timeline, menu);
+		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
 
